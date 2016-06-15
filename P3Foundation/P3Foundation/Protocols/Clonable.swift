804455538640c956baff5ea1d5593dc1ea0ad269
Oscar Swanros <oscar@swanros.com>
2016-06-15 17:02:21 -0500

    Add clonable protocol

---
+++ P3Foundation/P3Foundation/Protocols/Clonable.swift
@@ -0,0 +1,11 @@
+//
+//  Clonable.swift
+//  P3Foundation
+//
+//  Created by Oscar Swanros on 6/15/16.
+//  Copyright © 2016 Pacific3. All rights reserved.
+//
+
+public protocol Clonable {
+    func clone() -> Self?
+}
