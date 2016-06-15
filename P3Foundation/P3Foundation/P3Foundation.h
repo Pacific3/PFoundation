3b125aab708657d67bb5a3b0d6551334bf08fc06
Oscar Swanros <oscar@swanros.com>
2016-06-15 15:13:28 -0500

    Create new PFoundation framework

---
+++ P3Foundation/P3Foundation/P3Foundation.h
@@ -0,0 +1,19 @@
+//
+//  P3Foundation.h
+//  P3Foundation
+//
+//  Created by Oscar Swanros on 6/15/16.
+//  Copyright © 2016 Pacific3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for P3Foundation.
+FOUNDATION_EXPORT double P3FoundationVersionNumber;
+
+//! Project version string for P3Foundation.
+FOUNDATION_EXPORT const unsigned char P3FoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <P3Foundation/PublicHeader.h>
+
+
